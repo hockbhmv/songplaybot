08b6c3f7b8b8ddc6fa747300bcd32e8547464c9b
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-05 17:00:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ import requests
 
 bot = Client(
     'SongPlayRoBot',
-    bot_token = Config.BOT_TOKEN,
-    api_id = Config.API_ID,
-    api_hash = Config.API_HASH
+    bot_token = BOT_TOKEN,
+    api_id = API_ID,
+    api_hash = API_HASH
   )
 
 @Clinet.on_message(filters.command(start))
