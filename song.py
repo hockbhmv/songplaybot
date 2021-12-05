8c9bc3b1f5844da8c0d3d2f19316e916d7a5fc98
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-05 18:27:18 +0530

    Create song.py

---
+++ song.py
@@ -0,0 +1,71 @@
+
+
+
+
+@Client.on_message(filters.command("start"))
+async def start(bot, message):
+  await message.reply_text(text = f"hi how are you")
+  
+
+def yt_search(song):
+    videosSearch = VideosSearch(song, limit=1)
+    result = videosSearch.result()
+    if not result:
+        return False
+    else:
+        video_id = result["result"][0]["id"]
+        url = f"https://youtu.be/{video_id}"
+        return url
+      
+@Client.on_message(filters.command(["music", "song"]))
+async def song(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+ 
+    args = get_arg(message) + " " + "song"
+    if args.startswith(" "):
+        await message.reply("Enter a song name. Check /help")
+        return ""
+    status = await message.reply("<code>🚀 🔎 🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠... 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳️ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬</code> [🚀](https://telegra.ph/file/777939adbee7e24f55d5c.mp4)")
+    video_link = yt_search(args)
+    if not video_link:
+        await status.edit("✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.\n\n𝐓𝐫𝐲 𝐀𝐧𝐨𝐭𝐡𝐞𝐫 𝐊𝐞𝐲𝐰𝐨𝐫𝐤 𝐎𝐫 𝐌𝐚𝐲𝐛𝐞 𝐒𝐩𝐞𝐥𝐥.\n\nEg.`/song Faded`")
+        return ""
+    yt = YouTube(video_link)
+    results = []
+    count = 0
+    while len(results) == 0 and count < 6:
+        if count>0:
+            time.sleep(1)
+        results = YoutubeSearch(args, max_results=1).to_dict()
+        count += 1
+    title = results[0]["title"]
+    duration = results[0]["duration"]
+    views = results[0]["views"]
+    thumbnail = results[0]["thumbnails"][0]
+    audio = yt.streams.filter(only_audio=True).first()
+    thumb_name = f'thumb{message.message_id}.jpg' 
+    thumb = requests.get(thumbnail, allow_redirects=True)
+    open(thumb_name, 'wb').write(thumb.content)
+    cap =f" ❍ Title : <code>{title[:35]}</code>\n❍ duration : <code>{int(yt.length)}</code>\n❍ views : <code>{views}</code>\n\n❍ by @{temp.U_NAME}"
+    try:
+        
+        download = audio.download(filename=f"{str(user_id)}")
+    except Exception as ex:
+        await status.edit("Failed to download song 😶")
+        
+        return ""
+    rename = os.rename(download, f"{str(user_id)}.mp3")
+    await client.send_chat_action(message.chat.id, "upload_audio")
+    await client.send_audio(
+        chat_id=message.chat.id,
+        audio=f"{str(user_id)}.mp3",
+        duration=int(yt.length),
+        title=str(yt.title),
+        caption=cap,
+        thumb=thumb_name,
+        performer=str(yt.author),
+        reply_to_message_id=message.message_id,
+    )
+    await status.delete()
+    os.remove(f"{str(user_id)}.mp3")
