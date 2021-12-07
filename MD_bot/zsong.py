71bec7b0a4a28ec635e1216c793649677802cb69
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-07 17:25:02 +0530

    Update zsong.py

--- MD_bot/zsong.py
+++ MD_bot/zsong.py
@@ -63,7 +63,10 @@ async def song(client, message):
             time.sleep(1)
         results = YoutubeSearch(args, max_results=1).to_dict()
         count += 1
-    caps = CUSTOM_CAPTION.format( title = results[0]["title"],duration = results[0]["duration"],views = results[0]["views"],**locals())
+    caps =
+    title = results[0]["title"]
+    duration = results[0]["duration"]
+    views = results[0]["views"]
     thumbnail = results[0]["thumbnails"][0]
     audio = yt.streams.filter(only_audio=True).first()
     thumb_name = f'thumb{message.message_id}.jpg' 
@@ -84,7 +87,7 @@ async def song(client, message):
         audio=f"{str(user_id)}.mp3",
         duration=int(yt.length),
         title=str(yt.title),
-        caption=caps,
+        caption= CUSTOM_CAPTION.format(titles = title,view =views, duration = int(yt.length))
         thumb=thumb_name,
         performer=f"[MD MUSIC BOT]",
         reply_to_message_id=message.message_id,
