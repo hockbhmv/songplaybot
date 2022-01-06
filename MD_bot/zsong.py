009d33b617643e4628ba09eee4239465fb0bc5f3
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-06 19:17:05 +0530

    Update zsong.py

--- MD_bot/zsong.py
+++ MD_bot/zsong.py
@@ -43,7 +43,7 @@ def get_arg(message):
     return " ".join(split[1:])        
       
 @Client.on_message(filters.text & filters.group & filters.incoming)
-async def withoutcmd(bot, message):
+async def without_cmd(client, message):
    if message.text.startwith("/"):
       return 
    else: 
