c009da117d58e3f4439f6c6f62b79c39c9227721
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-08 00:04:10 +0530

    Update commands.py

--- MD_bot/commands.py
+++ MD_bot/commands.py
@@ -11,14 +11,14 @@ async def gstart(bot, cmd):
        reply_markup = InlineKeyboardMarkup(buttons)
        await message.reply(f"Hey,{cmd.chat.title}\ni am a song bot i can give song in your group")
        await asyncio.sleep(2) 
-        if not await db.get_chat(cmd.chat.id):
+       if not await db.get_chat(cmd.chat.id):
             total=await bot.get_chat_members_count(cmd.chat.id)
             channel_id = cmd.chat.id
             group_id = cmd.chat.id
             title = cmd.chat.title
             await db.add_chat(cmd.chat.id, cmd.chat.title)
             await bot.send_message(db, f"#new group:\nTitle - {}\nId - {}\nTotal members - {} added by - {}".format(cmd.chat.title,cmd.chat.id,total,"Unknown"))
-        return
+       return
    if not await db.is_user_exist(cmd.from_user.id): 
         await db.add_user(cmd.from_user.id, cmd.from_user.first_name)
         await bot.send_message(db, f"#NEWUSER: \nName - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\nID - {cmd.from_user.id}")
