2ca3a154ef0d63414e40eac5cf3342fe83df1870
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-09 00:38:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,5 +4,5 @@ from os import environ
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
-photo = (environ.get("PHOTOS", "")).split()
+PICS = (environ.get("PICS", "")).split()
 DB = environ['DB']
