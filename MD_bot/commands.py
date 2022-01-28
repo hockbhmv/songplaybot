5d2c95bcf74ae1f5d6a8522800ffa8b776326c7d
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-28 21:24:19 +0530

    Update commands.py

--- MD_bot/commands.py
+++ MD_bot/commands.py
@@ -59,7 +59,7 @@ async def withcmd(bot, message):
 
 @Client.on_callback_query(filters.regex(r"^done"))
 async def settings_query(bot, msg):
-   int, type, value = await msg.data.split('#')
+   int, type, value = msg.data.split('#')
    group = msg.message.chat.id
    if value==True:
       await save_group_settings(group, type, False)
