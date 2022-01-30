4d6e13e65e10f2e76a83bc9804f34e355358e3ee
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-30 14:00:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hockbhmv/songplaybot.git /songplaybot
+  git clone https://github.com/hockbhmv/songplaybot.git /songplaybot 
+  git clone https://github.com/subinps/VCPlayerBot.git /VCPlayerBot  
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /songplaybot
