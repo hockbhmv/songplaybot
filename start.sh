87bfc00457ed72dbe4f6897737eaa8f5fb8a3f63
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-08 14:17:34 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/hockbhmv/songplaybot /songplaybot
+else
+  echo "Cloning $UPSTREAM_REPO branch from Respository"
+  git clone https://github.com/hockbhmv/songplaybot -b $UPSTREAM_REPO /songplaybot
+fi
+cd /songplaybot
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
