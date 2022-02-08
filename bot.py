86e6c558e5f94ce2463d55e547c99e09446ef53a
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-08 11:11:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-from info import BOT_TOKEN, API_ID, API_HASH
+from info import BOT_TOKEN, API_ID, API_HASH, SESSION
 import pyromod.listen
 from pyrogram import Client
 
@@ -12,7 +12,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 bot = Client(
-    'SongPlayRoBot',
+    SESSION,
     bot_token = BOT_TOKEN,
     api_id = API_ID,
     api_hash = API_HASH,
