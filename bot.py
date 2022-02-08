a61a86d881621822a872cb70c85aef10a6e981af
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-08 19:07:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 from info import BOT_TOKEN, API_ID, API_HASH
 import pyromod.listen
 from pyrogram import Client
+from user import User
 
 import logging
 logging.basicConfig(level=logging.DEBUG,
@@ -11,19 +12,32 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 
-bot = Client(
-    'SongPlayRoBot',
-    bot_token = BOT_TOKEN,
-    api_id = API_ID,
-    api_hash = API_HASH,
-    plugins={"root": "MD_bot"},
-    workers=100
-  )
-
-
+class Bot(Client):
+    USER: User = None
+    USER_ID: int = None
+    
+    def __init__(self):
+        super().__init__(
+        "bot",
+        bot_token = BOT_TOKEN,
+        api_id = API_ID,
+        api_hash = API_HASH,
+        plugins={"root": "MD_bot"},
+        workers=20
+        )
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.set_parse_mode("html")
+        self.USER, self.USER_ID = await User().start()
+        print(f'bot {usr_bot_me.username} started')  
+        
+    async def stop(self, *args):
+        await super().stop()
+.       print("bot stopped")
             
         
     
-bot.run()
+Bot().run()
     
     
