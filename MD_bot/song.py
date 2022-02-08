15b2949b362d5a3aead2ca08e23f933c6ab04157
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-08 20:19:03 +0530

    Update song.py

--- MD_bot/song.py
+++ MD_bot/song.py
@@ -39,8 +39,8 @@ async def search(bot, message):
     query = message.text
     chat = -1001662995429
     if chat:
-       async for message in bot.USER.search_messages(chat, query=query, limit=1):
-            await message.copy(message.from_user.id)  if message else await message.reply_text("nothing")
+       async for msg in bot.USER.search_messages(chat, query=query, limit=1):
+            await msg.copy(message.from_user.id)  if msg else await message.reply_text("nothing")
    
 @Client.on_message(filters.text & filters.group & filters.incoming)
 async def song(client, message):
