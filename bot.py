9997d3c3b9ac7e9c377ba00b921a5e0be595abd3
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-08 11:23:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from info import BOT_TOKEN, API_ID, API_HASH, SESSION
 import pyromod.listen
 from pyrogram import Client
-
+from user import USER, USER_ID
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -10,20 +10,27 @@ logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-
-bot = Client(
-    SESSION,
-    bot_token = BOT_TOKEN,
-    api_id = API_ID,
-    api_hash = API_HASH,
-    plugins={"root": "MD_bot"},
-    workers=100
-  )
-
-
+class Bot(Client):
+    USER: User = None
+    USER_ID: int = None
+    
+    def __init__(self):
+        super().__init__(
+        SESSION,
+        bot_token = BOT_TOKEN,
+        api_id = API_ID,
+        api_hash = API_HASH,
+        plugins={"root": "MD_bot"},
+        workers=100
+        )
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.set_parse_mode("html")
+        print(f'bot {usr_bot_me} started')
+        self.USER, self.USER_ID = await User().start()
             
+    async def stop(self, *args):
+        await super().stop()
         
-    
-bot.run()
-    
     
