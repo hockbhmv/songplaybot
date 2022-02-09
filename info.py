9a2cf3c0d1180f93a2b86977eafc102d15e7701a
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-09 15:03:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,5 +5,6 @@ API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 SESSION = environ['SESSION']
 BOT_TOKEN = environ['BOT_TOKEN']
+CHANNEL = int(environ.get("CHANNEL", "")
 PICS = (environ.get("PICS", "")).split()
 DB = environ['DB']
