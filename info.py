f77a245ec49d30ebb6680ef0aa39a0016936085d
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-10 23:16:01 +0530

    Remove user bot

--- info.py
+++ info.py
@@ -3,7 +3,6 @@ from os import environ
 
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
-SESSION = environ['SESSION']
 BOT_TOKEN = environ['BOT_TOKEN']
 CHANNEL = int(environ.get('CHANNEL', '-1001662995429'))
 PICS = (environ.get("PICS", "")).split()
