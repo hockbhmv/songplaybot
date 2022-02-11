60e519611c0c7004ed8db40ae6fd2d8d6e514ce1
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-11 16:39:03 +0530

    Update song.py

--- MD_bot/song.py
+++ MD_bot/song.py
@@ -68,7 +68,7 @@ async def song(client, message):
     files, offset, total_results = await get_search_results(str(yt.title), offset=1, filter=True)
     if files:
        for file in files:
-         if file.file_name==f"{str(yt.title)}.mp3":
+         if str(file.file_name)==f"{str(yt.title)}.mp3":
              xx = await client.send_cached_media(chat_id=message.chat.id,file_id=file.file_id, caption=file.caption, reply_to_message_id= message.message_id)
              can = [[InlineKeyboardButton('🔰 SEND IN MY PM 🔰', callback_data=f"pm#{xx.message_id}#{message.chat.id}")]]
              await xx.edit_reply_markup(InlineKeyboardMarkup(can))
