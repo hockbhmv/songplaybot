1827144d1015b231b93db33aef92d8f3d99f0554
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-11 19:54:02 +0530

    API change

--- MD_bot/Lyrics.py
+++ MD_bot/Lyrics.py
@@ -5,14 +5,9 @@ from pyrogram import filters
 from lyricsgenius import genius
 from pyrogram import Client as pbot
 
-#API = "https://apis.xditya.me/lyrics?song="
-api = genius.Genius("LTjINTL7fUCR16PdCLVHK339v4vxPtC8Pe70aDW7_vA7BlSbABKcnddm5ZxdomB2",verbose=False)
+api = genius.Genius("OXaVabSRKQLqwpiYOn-E4Y7k3wj-TNdL5RfDPXlnXhCErbcqVvdCF-WnMR5TBctI",verbose=False)
 
-def search(song):
-        r = requests.get(API + song)
-        find = r.json()
-        return find
-       
+
 def lyrics(song):
         lyric = api.search_song(song)
         lyrics = lyric.lyrics
@@ -22,7 +17,7 @@ def lyrics(song):
         return text
 
 
-@pbot.on_message(filters.command(["lyric", "lyrics"]) & filters.group)
+@pbot.on_message(filters.command(["lyric", "lyrics"]))
 async def _(client, message):
    if ' ' in message.text:
       r, query = message.text.split(None, 1)
@@ -34,3 +29,4 @@ async def _(client, message):
       except Exception as e:
          await message.reply_text(f"I Can't Find A Song With `{query}`", quote = True)
          print(f"{e}")
+   
