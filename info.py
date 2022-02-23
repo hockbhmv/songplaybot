4e266338dd39b46e0b0dea28fef5d014cd44ae4d
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-23 22:47:50 +0530

    Export

--- info.py
+++ info.py
@@ -6,4 +6,5 @@ API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 CHANNEL = int(environ.get('CHANNEL', '-1001662995429'))
 PICS = (environ.get("PICS", "")).split()
+MONGODB_URL = environ['MONGODB_URL']
 DB = environ['DB']
