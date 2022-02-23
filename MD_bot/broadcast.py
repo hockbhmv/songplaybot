9cea1758b52047127782efb01009b45ba6dcc54d
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-23 23:54:37 +0530

    Fix

--- MD_bot/broadcast.py
+++ MD_bot/broadcast.py
@@ -1,38 +1,46 @@
 import asyncio 
 import time, datetime
-from pyrogram import Client, filters
-from .db import total_users, total_groups, all_users, all_groups
+from pyrogram import Client, filters 
+from .database import get_all_users, get_all_chats, total_users_count, total_chat_count
+from db import total_users, total_groups, new_user, new_group
 from info import MONGODB_URL, DB
 
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
+@Client.on_message(filters.command("broadcast"))
 async def verupikkals(bot, message):
     i, use = message.text.split(None, 1)
     if use=="users":
-        total = await total_users()
-        users = await all_users()
-        total_users = total
+        total = await total_users_count()
+        users = await get_all_users()
+        ntotal = await total_users()
+        total_users = total 
+        name = "name"
+        new_chat = new_user
     else:
-        total = await total_groups()
-        users = await all_groups()
-        total_users = total
-    b_msg = message.reply_to_message
+        total = await total_chat_count()
+        users = await get_all_chats()
+        ntotal = await total_groups()
+        total_users = total 
+        name = "title"
+        new_chat=new_group
     sts = await message.reply_text(
         text='Broadcasting your messages...'
     )
     start_time = time.time()
     
     done = 0
-    blocked = 0
-    deleted = 0
-    failed =0
-
-    success = 0
+    old = 0
+    new = 0
     async for user in users:
-        
+        i, use= await new_chat(int(user["id"]), user[name])
+        if use=="in":
+           old+=1
+        else:
+           new+=1
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {new}\nalready: {old}\n\nnew added: {ntotal}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {new}\n\n: {old}\nnew added: {ntotal}")
+
+    
