1cf3aaa002d7376d8997a4640e79dc82a71703c4
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-25 21:05:03 +0530

    Fix

--- info.py
+++ info.py
@@ -5,6 +5,5 @@ API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 CHANNEL = int(environ.get('CHANNEL', '-1001662995429'))
-PICS = (environ.get("PICS", "")).split()
+PICS = (environ.get("PICS", None)).split()
 MONGODB_URL = environ['MONGODB_URL']
-DB = environ['DB']
