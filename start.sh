6ef05c196df2d4787d875d0f14c78cc7c6ea2925
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-01 23:28:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hockbhmv/songplaybot /songplaybot
+  git clone https://github.com/hockbhmv/video-stream /video-stream 
 else
   echo "Cloning $UPSTREAM_REPO branch from Respository"
-  git clone https://github.com/hockbhmv/songplaybot -b $UPSTREAM_REPO /songplaybot
+  git clone $UPSTREAM_REPO -b $BRANCH /video-stream
 fi
-cd /songplaybot
+cd /video-stream
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python3 main.py
